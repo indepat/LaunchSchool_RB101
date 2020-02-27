1e191f841c5a5f7ab43e18422e73ec6b5e0e6f3f
Patrick Indekwe <indepat1@gmail.com>
2020-02-27 12:08:55 -0500

    :vulcan_salute: refactoring

--- lesson_6/tictactoe.rb
+++ lesson_6/tictactoe.rb
@@ -1,13 +1,13 @@
 =begin
 
-TIC TAC TOE 
+TIC TAC TOE
 
-This program will describe how to make a tic tac toe game. 
+This program will describe how to make a tic tac toe game.
 
 Instructions:
 
 1. Display the initial empty 3x3 board
-2. Ask the user to mark a square 
+2. Ask the user to mark a square
 3. Computer marks a square
 4. Display the updated board state
 5. If winner, display the winner
@@ -19,6 +19,9 @@ Instructions:
 =end
 
 require 'pry'
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
+                [[1, 5, 9], [3, 5, 7]] # diagnals
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
@@ -27,6 +30,7 @@ def prompt(message)
   puts "=> #{message}"
 end
 
+# rubocop:disable Metrics/AbcSize
 def display_board(brd)
   system 'clear'
   puts "You're #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}"
@@ -43,17 +47,17 @@ def display_board(brd)
   puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}  "
   puts "     |     |"
   puts ""
-  
 end
+# rubocop:enable Metrics/AbcSize
 
 def initialize_board
   new_board = {}
-  (1..9).each { |num| new_board[num] = INITIAL_MARKER}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
 def empty_squares(brd)
-  brd.keys.select {|num| brd[num] == INITIAL_MARKER}
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
 def player_places_piece!(brd)
@@ -62,8 +66,8 @@ def player_places_piece!(brd)
     prompt "Choose a square (#{empty_squares(brd).join(', ')}): "
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
-   prompt "Sorry, that's not a valid choice."
-  end 
+    prompt "Sorry, that's not a valid choice."
+  end
   brd[square] = PLAYER_MARKER
 end
 
@@ -80,34 +84,37 @@ def someone_won?(brd)
   !!detect_winner(brd)
 end
 
+# rubocop:disable Metrics/MethodLength
 def detect_winner(brd)
-  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
-                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols 
-                  [[1, 5, 9], [3, 5, 7]]             # diagnals 
-  winning_lines.each do |line|
+  WINNING_LINES.each do |line|
+=begin
     if brd[line[0]] == PLAYER_MARKER &&
        brd[line[1]] == PLAYER_MARKER &&
        brd[line[2]] == PLAYER_MARKER
-       return 'Player'
+      return 'Player'
     elsif brd[line[0]] == COMPUTER_MARKER &&
-        brd[line[1]] == COMPUTER_MARKER &&
-        brd[line[2]] == COMPUTER_MARKER
+          brd[line[1]] == COMPUTER_MARKER &&
+          brd[line[2]] == COMPUTER_MARKER
+      return 'Computer'
+    end
+=end
+    if brd.values_at(line[0], line[1], line[2]).count(PLAYER_MARKER) == 3
+      return 'Player'
+    elsif brd.values_at(line[0], line[1], line[2]).count(COMPUTER_MARKER) == 3
       return 'Computer'
     end
   end
   nil
 end
+# rubocop:enable Metrics/MethodLength
 
 loop do
-
   board = initialize_board
 
   loop do
     display_board(board)
-
     player_places_piece!(board)
-      break if someone_won?(board) || board_full?(board)
-
+    break if someone_won?(board) || board_full?(board)
     computer_places_piece!(board)
     break if someone_won?(board) || board_full?(board)
   end
@@ -121,7 +128,7 @@ loop do
 
   prompt "play again? (y or n)"
   answer = gets.chomp
-  break unless answer.downcase.start_with? ('y')
+  break unless answer.downcase.start_with?('y')
 end
 
 prompt "Thanks for playing tic tac toe! Good bye!"
